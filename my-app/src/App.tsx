a87f401fb8cf2ba785661231095254742643de33
DABukatin <DABukatin@mtsbank.ru>
2024-02-14 14:50:10 +0300

    init

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {AppBar} from "@mui/material";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <AppBar>Hello</AppBar>
+      </header>
+    </div>
+  );
+}
+
+export default App;
